d31893f3b998227f3236d476f576da0030c95923
Bruno Bornsztein <bruno.bornsztein@gmail.com>
2021-06-08 11:31:35 -0500

    fix: update for api v3

--- lib/omniauth/strategies/constantcontact2.rb
+++ lib/omniauth/strategies/constantcontact2.rb
@@ -5,55 +5,18 @@ require 'multi_xml'
 module OmniAuth
   module Strategies
     class ConstantContact2 < OmniAuth::Strategies::OAuth2
-      
-      DEFAULT_RESPONSE_TYPE = 'code'
-      DEFAULT_GRANT = 'authorization_code'
-      API_PATH = 'https://api.constantcontact.com/v2'
-      
+            
       option :name, "constantcontact"
 
       option :client_options, {
-          :site => 'https://oauth2.constantcontact.com',
-          :authorize_url => '/oauth2/oauth/siteowner/authorize',
-          :token_url => '/oauth2/oauth/token'
+        :authorize_url => 'https://api.cc.email/v3/idfed',
+        :token_url => 'https://idfed.constantcontact.com/as/token.oauth2'
       }
 
-      def authorize_params
-        super.tap do |params|
-          params[:response_type] ||= DEFAULT_RESPONSE_TYPE
-          params[:client_id] = client.id
-        end
+      def callback_url
+        full_host + script_name + callback_path
       end
 
-      def token_params
-        super.tap do |params|
-          params[:grant_type] ||= DEFAULT_GRANT
-          params[:client_id] = client.id
-          params[:client_secret] = client.secret
-        end
-      end
-
-      uid do
-        request.params['username']
-      end
-
-      info do
-        {
-          email: raw_info['email']
-        }
-      end
-
-      extra do
-        { 'raw_info' => raw_info }
-      end
-
-      def raw_info
-        options = {
-          params: {api_key: client.id},
-          headers: {'Authorization' => "Bearer #{access_token.token}"}
-        }
-        @raw_info ||= JSON.parse(access_token.get("#{API_PATH}/account/info", options).body)
-      end
     end
   end
 end
